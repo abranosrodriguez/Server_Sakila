ffdaeaaccf6fe7eb40b4f28b30d2b83e7393f0ff
abranosrodriguez <adrianbraosrodriguez@gmail.com>
2025-06-16 12:13:32 +0200

    Added dbConfig

--- backend/src/types/express-session.ts
+++ backend/src/types/express-session.ts
@@ -3,6 +3,12 @@ import "express-session";
 declare module "express-session" {
   interface SessionData {
     isAuthenticated?: boolean;
+    dbConfig?: {
+      host: string;
+      database: string;
+      user: string;
+      password: string;
+    };
     // Agrega aquí más propiedades personalizadas si necesitas
   }
 }
