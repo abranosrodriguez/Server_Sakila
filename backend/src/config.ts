2ec67d76898852a7cc63359f412575c776f314fc
abranosrodriguez <adrianbraosrodriguez@gmail.com>
2025-06-16 13:19:49 +0200

    cambio config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,8 +4,6 @@ export const dbConfig = {
   host: "bdxzlfnl0hkj6tubc5bu-mysql.services.clever-cloud.com",
   user: "urtbolltvvyvy4yr",
   port: 3306,
-  password: isWindows
-    ? process.env.DB_PASSWORD_WINDOWS || "NNwetQBHy7Q5l7eN0njj"
-    : "mariadb",
+  password: "NNwetQBHy7Q5l7eN0njj",
   database: "bdxzlfnl0hkj6tubc5bu",
 };
