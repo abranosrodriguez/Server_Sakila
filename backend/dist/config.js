55c02e0dc73b084039ad6e7ee2e1a142258c4279
abranosrodriguez <adrianbraosrodriguez@gmail.com>
2025-06-16 13:21:57 +0200

    Modified Config

--- backend/dist/config.js
+++ backend/dist/config.js
@@ -6,8 +6,6 @@ exports.dbConfig = {
     host: "bdxzlfnl0hkj6tubc5bu-mysql.services.clever-cloud.com",
     user: "urtbolltvvyvy4yr",
     port: 3306,
-    password: isWindows
-        ? process.env.DB_PASSWORD_WINDOWS || "NNwetQBHy7Q5l7eN0njj"
-        : "mariadb",
+    password: "NNwetQBHy7Q5l7eN0njj",
     database: "bdxzlfnl0hkj6tubc5bu",
 };
